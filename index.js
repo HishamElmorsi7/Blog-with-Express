8d6e165bce35b66d5fa910384adbc81c7f66d3cc
HishamElmorsi7 <hi7u3xe@gmail.com>
2023-01-28 18:31:19 +0200

    Simple routing system using switch

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ const http = require('http');
 // requiring fs to read the html files
 const fs = require('fs')
 
+
 const server = http.createServer((req, res) => {
+
+    let path = './views/';
+    let url = req.url;
+
+    switch(url) {
+        case '/':
+            path += 'index.html'
+            break;
+        case '/contact':
+            path += 'contact.html'
+            break
+        default:
+            path += '404.html'
+    }
 
     res.setHeader('content-type', 'text/html');
     // reading the file and write it then to the browser
-    fs.readFile('./views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if(err) {
             console.log('There is an error')
             res.end();
