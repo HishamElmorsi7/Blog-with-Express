0cba78007bb514e9caa1264e03a366783b5b6c73
HishamElmorsi7 <hi7u3xe@gmail.com>
2023-01-28 10:01:48 +0200

    separating html in file in views folder

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+// require http core module to be able to create a js server
 const http = require('http');
+// requiring fs to read the html files
+const fs = require('fs')
+
 const server = http.createServer((req, res) => {
-    res.setHeader('content-type', 'text/html')
-    res.write('<head> <link rel = "stylesheet" ref = "#x"</head>')
-    res.write('<h1>we are going</h1>')
-    res.end()
+
+    res.setHeader('content-type', 'text/html');
+    // reading the file and write it then to the browser
+    fs.readFile('./views/index.html', (err, data) => {
+        if(err) {
+            console.log('There is an error')
+            res.end();
+        } else {
+            res.end(data)
+        }
+    })
 })
 
 server.listen(3000, 'localhost', () => {
