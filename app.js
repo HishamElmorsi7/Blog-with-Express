05ed37b945b7e05cdced431882acb6a170046b57
HishamElmorsi7 <hi7u3xe@gmail.com>
2023-01-30 03:16:11 +0200

    routing the ejs views using express for rendering

--- app.js
+++ app.js
@@ -2,21 +2,23 @@ const express = require('express')
 
 const app = express();
 
+app.set('view engine', 'ejs')
+
 app.listen(3000)
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname})
+    res.render('index')
 })
 
-app.get('/contact', (req, res) => {
-    res.sendFile('./views/contact.html', {root: __dirname})
+app.get('/about', (req, res) => {
+    res.render('about')
 })
 
-app.get('/contact-me', (req, res) => {
-    res.redirect('/contact')
+app.get('/blogs/create', (req, res) => {
+    res.render('create')
 })
 
 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname})
+    res.status(404).render('404')
 })
